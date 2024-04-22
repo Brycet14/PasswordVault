f801d88d77534d5e085e13d1b3ef57b8ad960ae5
Brycet14 <111893439+Brycet14@users.noreply.github.com>
2024-04-22 08:41:07 -0600

    Updated sign in functions

--- vault1/src/main/java/passwordvault/Main.java
+++ vault1/src/main/java/passwordvault/Main.java
@@ -18,37 +18,49 @@ public class Main {
         int start, select;
         boolean signedIn = false;
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the secure password vault.\nTo sign in, press 1: \nTo create an account, press 2: ");
+        System.out.println(
+                "Welcome to the secure password vault.\nTo sign in, press 1: \nTo create an account, press 2: ");
         start = sc.nextInt();
 
         // Encrypt.encrypt(path);
+        // Decrypt.decrypt("/Users/9709796/Documents/Personal Project/PasswordVault/users.db");
         if (start == 1) {
             System.out.println("Enter your email: ");
+            sc.nextLine();
             sEmail = sc.nextLine();
             System.out.println("Enter your password: ");
             sPswrd = sc.nextLine();
+            checkSignIn(sEmail, sPswrd);
 
             if (checkSignIn(sEmail, sPswrd)) {
                 signedIn = true;
                 System.out.println("Sign in successful! Welcome to your vault!");
-                Decrypt.decrypt(path + "/" + );
+                Decrypt.decrypt(path + "/" + getVaultName(sEmail));
                 while (signedIn) {
-                    System.out.println("To find a password, press 1. \nTo add a password to your vault, press 2 \nTo exit the vault, press 3");
+                    System.out.println(
+                            "To find a password, press 1. \nTo add a password to your vault, press 2 \nTo exit the vault, press 3");
                     select = sc.nextInt();
-                    if(select == 1) {
-
-                    } else if(select == 2) {
-
+                    if (select == 1) {
+                        sc.nextLine();
+                        System.out.println("Enter the websites name: ");
+                    } else if (select == 2) {
+                        sc.nextLine();
+                        System.out.println("Enter the websites name: ");
+                        String website = sc.nextLine();
+                        System.out.println("Enter your username for the website: ");
+                        String wUsername = sc.nextLine();
+                        System.out.println("Enter your password for the website: ");
+                        String wPswrd = sc.nextLine();
                     } else if (select == 3) {
                         break;
                     }
                 }
             } else {
-                System.out.println("Incorrect email or password. Please try again.");
+                System.out.println("Incorrect email or password.");
             }
-
         } else if (start == 2) {
             System.out.println("Please enter an email: ");
+            sc.nextLine();
             email = sc.nextLine();
             System.out.println("Please enter a password: ");
             uPswrd = sc.nextLine();
@@ -59,12 +71,12 @@ public class Main {
                 System.out.println("Please name your vault: ");
                 vaultName = sc.nextLine();
 
-                storeCred(email, fPswrd, vaultName);
+                storeUserCred(email, fPswrd, vaultName);
                 createVault(vaultName);
             } else {
                 System.out.println("Passwords did not match! Try again.");
             }
-        } 
+        }
         sc.close();
     }
 
@@ -72,51 +84,109 @@ public class Main {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
-    private static void storeCred(String email, String pswrd, String vaultName) {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db")) {
-            String query = "INSERT INTO users (email, password, vaultName) VALUES (?, ?, ?)";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, email);
-                preparedStatement.setString(2, pswrd);
-                preparedStatement.setString(3, vaultName);
-                preparedStatement.executeUpdate();
+    private static void storeUserCred(String email, String pswrd, String vaultName) {
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db")) {
+                String query = "INSERT INTO users (email, password, vaultName) VALUES (?, ?, ?)";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                    preparedStatement.setString(1, email);
+                    preparedStatement.setString(2, pswrd);
+                    preparedStatement.setString(3, vaultName);
+                    preparedStatement.executeUpdate();
+                } catch (SQLException e) {
+                    System.err.println("Error storing credentials: " + e.getMessage());
+                }
             } catch (SQLException e) {
                 System.err.println("Error storing credentials: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.err.println("Error storing credentials: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC Driver not found: " + e.getMessage());
         }
     }
 
     private static void createVault(String vaultName) {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + vaultName + ".db")) {
-            String query = "CREATE TABLE IF NOT EXISTS websites (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, username TEXT, password TEXT)";
-            try (Statement statement = connection.createStatement()) {
-                statement.executeUpdate(query);
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:" + vaultName + ".db")) {
+                String query = "CREATE TABLE IF NOT EXISTS storage (id INTEGER PRIMARY KEY AUTOINCREMENT, website TEXT, username TEXT, password TEXT)";
+                try (Statement statement = connection.createStatement()) {
+                    statement.executeUpdate(query);
+                } catch (SQLException e) {
+                    System.err.println("Error creating Vault: " + e.getMessage());
+                }
             } catch (SQLException e) {
                 System.err.println("Error creating Vault: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.err.println("Error creating Vault: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC Driver not found: " + e.getMessage());
         }
     }
 
     private static boolean checkSignIn(String email, String pswrd) {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db")) {
-            String query = "SELECT password FROM users WHERE email = ?";
-            try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
-                preparedStatement.setString(1, query);
-                ResultSet resultSet = preparedStatement.executeQuery();
-                if (resultSet.next()) {
-                    String hashedPassword = resultSet.getString("password");
-                    return BCrypt.checkpw(pswrd, hashedPassword);
+        try {
+            Class.forName("org.sqlite.JDBC");
+
+            try (Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db")) {
+                String query = "SELECT password FROM users WHERE email = ?";
+                try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+                    preparedStatement.setString(1, email);
+                    ResultSet resultSet = preparedStatement.executeQuery();
+                    if (resultSet.next()) {
+                        String hashedPassword = resultSet.getString("password");
+                        return BCrypt.checkpw(pswrd, hashedPassword);
+                    } else {
+                        System.out.println("User with email '" + email + "' not found.");
+                    }
+                } catch (SQLException e) {
+                    System.err.println("Error checking sign-in credentials: " + e.getMessage());
                 }
             } catch (SQLException e) {
-                System.err.println("Error checking sign-in credentials: " + e.getMessage());
+                System.err.println("Error establishing connection: " + e.getMessage());
             }
-        } catch (SQLException e) {
-            System.err.println("Error establishing connection: " + e.getMessage());
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC driver not found: " + e.getMessage());
         }
         return false;
+    }
+
+    private static String getVaultName(String email) {
+        String vaultName = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            try {
+                Connection connection = DriverManager.getConnection("jdbc:sqlite:users.db");
+                String sql = "SELECT vaultName FROM users WHERE email = ?";
+                PreparedStatement statement = connection.prepareStatement(sql);
+                statement.setString(1, email);
+
+                ResultSet resultSet = statement.executeQuery();
+
+                if (resultSet.next()) {
+                    vaultName = resultSet.getString("vaultName");
+                }
+
+                resultSet.close();
+                statement.close();
+                connection.close();
+            } catch (SQLException e) {
+                System.err.println("Error fetching vault name" + e.getMessage());
+                e.printStackTrace();
+            }
+        } catch (ClassNotFoundException e) {
+            System.err.println("SQLite JDBC driver not found: " + e.getMessage());
+        }
+        return vaultName;
+    }
+
+    private static void storeCredentials(String website, String username, String pswrd) {
+        try{
+            Class.forName("org.sqlite.JDBC");
+            try(Connection connection = DriverManager.getConnection("jdbc:sqlite:" + getVaultName(sEmail) + ".db")) {
+                
+            }
+        }
     }
 }
